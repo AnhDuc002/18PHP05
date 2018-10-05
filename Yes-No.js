ca46d31d98c0772e3d041ef2f28887f02ca16b76
Harry <henryforce45@gmail.com>
2018-10-05 18:49:04 +0700

    Menu

---
+++ Yes-No.js
@@ -0,0 +1,8 @@
+//onmouseover event
+function show(){
+	document.getElementById('no').innerHTML = 'YES';
+}
+//onmouseout event
+function showOld(){
+	document.getElementById('no').innerHTML = 'NO';
+}
